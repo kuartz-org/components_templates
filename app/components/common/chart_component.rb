f4da915405abc2627675384bc21ce85d4b2fd0ad
Guillaume CABANEL <guillaume@kuartz.fr>
2023-09-07 08:58:33 +0200

    refacto chart component

--- app/components/common/chart_component.rb
+++ app/components/common/chart_component.rb
@@ -2,6 +2,11 @@
 
 module Common
   class ChartComponent < ViewComponent::Base
+    slim_template <<~SLIM
+      section data-controller="chart" data-chart-payload-value=(payload)
+        canvas data-chart-target="canvas"
+    SLIM
+
     def initialize(payload)
       @payload = payload
     end
--- template.rb
+++ template.rb
@@ -3,4 +3,6 @@ def source_paths
 end
 
 directory "app/components/common", force: true
+directory "app/javascript/controllers", force: true
 directory "test/components/previews/common", force: true
+directory "test/components/docs", force: true
