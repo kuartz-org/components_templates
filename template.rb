75efa90089d468b1264a46f0712450e122b92665
Guillaume CABANEL <guillaume@kuartz.fr>
2023-09-12 13:37:25 +0200

    move chartjs setup in rails template

--- template.rb
+++ template.rb
@@ -6,8 +6,3 @@ directory "app/components/common", force: true
 directory "app/javascript/controllers", force: true
 directory "test/components/previews/common", force: true
 directory "test/components/docs", force: true
-
-run "importmap pin 'chart.js'"
-run "importmap pin 'date-fns'"
-run "importmap pin 'date-fns/locale'"
-run "importmap pin 'chartjs-adapter-date-fns'"
