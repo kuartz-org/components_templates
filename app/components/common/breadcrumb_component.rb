0857e21cf4fb6a5c23148f97bcd4996d01e0c482
Guillaume CABANEL <guillaume@kuartz.fr>
2023-09-12 14:36:20 +0200

    add breadcrumb

---
+++ app/components/common/breadcrumb_component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Common
+  class BreadcrumbComponent < ViewComponent::Base
+    renders_many :items, "ItemComponent"
+
+    class ItemComponent < ViewComponent::Base
+      slim_template <<~SLIM
+        li.group
+          .flex.items-center.text-sm.font-medium.text-gray-500.space-x-4.group-first:-ml-4
+            i.fa-solid.fa-chevron-right.fa-xs.group-first:hidden
+            = content
+      SLIM
+    end
+  end
+end
